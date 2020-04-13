f54cf88a4862a7986e5ad0a2d09f9a2413ea1d37
danielstrzelecki99 <63561081+danielstrzelecki99@users.noreply.github.com>
2020-04-13 19:38:57 +0200

    Update Punkty.cpp

--- Punkty.cpp
+++ Punkty.cpp
@@ -1,6 +1,16 @@
 #include "Punkty.h"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
+
+Punkty::Punkty()
+{
+	czc.loadFromFile("font.ttf") ;
+	napis.setCharacterSize(46);
+	napis.setPosition(920, 10);
+	napis.setFillColor(Color::Black);
+	napis.setFont(czc);
+}
 
 void Punkty::draw(RenderTarget& target, RenderStates state) const
 {
@@ -9,5 +19,13 @@ void Punkty::draw(RenderTarget& target, RenderStates state) const
 
 void Punkty::update(int pkt)
 {
-	napis = to_string(pkt);
+	napis.setString("Punkty: " + to_string(pkt));
+}
+
+void Punkty::przegrana(int pkt)
+{
+	napis.setCharacterSize(70);
+	napis.setFillColor(Color::White);
+	napis.setPosition(450, 260);
+	napis.setString("PRZEGRANA\n Punkty: " + to_string(pkt));
 }
